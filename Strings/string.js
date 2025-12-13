284dc4dd7c886debb6f4255b80cae3b4eb10324e
Tayyab Ali <freelancewithtayyab@gmail.com>
2025-12-13 13:37:35 +0500

    Solve string palindmore problem

--- Strings/string.js
+++ Strings/string.js
@@ -18,3 +18,38 @@ let reverseStr = (str) => {
 
 console.log(reverseStr('Hello'))
 console.log(reverseStr('string'))
+
+
+
+// PROBLM 2 Valid Palindrome — Problem Description
+
+// You are given a string,
+
+// Your task is to check if the string reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+
+// Rules
+
+// Ignore all non-alphanumeric characters (letters and numbers only)
+// Ignore uppercase/lowercase differences
+// Return true if the string is a palindrome, false otherwise
+
+
+function Checkpalindrome(str) {
+    let cleaned = ''
+    for (let i = 0; i < str.length; i++) {
+        let ch = str[i].toLowerCase()
+        if (ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9') {
+            cleaned += ch
+        }
+    }
+
+    let reversed = '';
+    for (let i = cleaned.length - 1; i >= 0; i--) {
+        reversed += cleaned[i];
+    }
+    return reversed === cleaned
+}
+
+console.log(Checkpalindrome("A man, a plan, a canal: Panama"))
+console.log(Checkpalindrome("raceacar"))
+console.log(Checkpalindrome("Hello"))
